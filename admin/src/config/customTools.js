14e62069097cd7fc5fdea8cfee00ba87a5b2e7a4
Markus Mälzer <markus.maelzer@gmail.com>
2023-02-22 11:32:24 +0100

    add nessesary plugins for titles

--- admin/src/config/customTools.js
+++ admin/src/config/customTools.js
@@ -8,13 +8,63 @@ import Code from "@editorjs/code";
 import LinkTool from "@editorjs/link";
 import Raw from "@editorjs/raw";
 import Header from "@editorjs/header";
+import Paragraph from "@editorjs/paragraph";
 import Quote from "@editorjs/quote";
 import Marker from "@editorjs/marker";
 import CheckList from "@editorjs/checklist";
 import Delimiter from "@editorjs/delimiter";
 import InlineCode from "@editorjs/inline-code";
+import AlignmentTuneTool from "editorjs-text-alignment-blocktune";
+import ColorPlugin from "editorjs-text-color-plugin";
+import ChangeCase from "editorjs-change-case";
 
 const customTools = {
+  header: {
+    class: Header,
+    inlineToolbar: true,
+    tunes: ["anyTuneName"],
+  },
+  paragraph: {
+    class: Paragraph,
+    inlineToolbar: true,
+    tunes: ["anyTuneName"],
+  },
+  anyTuneName: {
+    class: AlignmentTuneTool,
+    config: {
+      default: "left",
+      blocks: {
+        header: "left",
+      },
+    },
+  },
+  changeCase: {
+    class: ChangeCase,
+  },
+  Color: {
+    class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+    config: {
+      colorCollections: [
+        "#50BEC8",
+        "#1B7780",
+        "#222222",
+        "#464646",
+        "#e1e1e1",
+        "#FFFFFF",
+        "#000000",
+        "#2DC26B",
+        "#F1C40F",
+        "#E03E2D",
+        "#B96AD9",
+        "3598DB",
+        "169179",
+        "E67E23",
+      ],
+      defaultColor: "#222222",
+      type: "text",
+      customPicker: true, // add a button to allow selecting any colour
+    },
+  },
   // embed: Embed,
   // table: {
   //   class: Table,
@@ -43,10 +93,7 @@ const customTools = {
   //   class: Raw,
   //   inlineToolbar: true,
   // },
-  // header: {
-  //   class: Header,
-  //   inlineToolbar: true,
-  // },
+
   // quote: {
   //   class: Quote,
   //   inlineToolbar: true,
